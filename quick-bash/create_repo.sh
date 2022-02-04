fd141277e18a10456a3bf67d657cf4e5a5a58dbd
Toribash <naotta@gmail.com>
2022-02-04 19:47:47 +0300

    add bash repo commands

---
+++ quick-bash/create_repo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# arg $1 as repo name
+# arg $2 as repo storage
+
+myrepo=$1
+
+cd $myrepo
+
+git init --shared=true
+git add .
+git commit -m "my first commit"
+cd ..
+git clone --bare $myrepo $myrepo.git
+
+mv myrepo.git $2 
+
+rm -rf $myrepo
+
+git clone git clone ssh://git@127.0.0.1:2222/git-server/repos/$myrepo.git
+
+echo "Done"
+
