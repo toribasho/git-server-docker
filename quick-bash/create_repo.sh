f3763809b56f56cdcdf355534e7a6c0a4aa05a2c
Toribash <naotta@gmail.com>
2022-02-07 23:30:14 +0300

    add repo path as param

--- quick-bash/create_repo.sh
+++ quick-bash/create_repo.sh
@@ -2,8 +2,11 @@
 
 # arg $1 as repo name
 # arg $2 as repo storage
+# arg $3 as repo path
 
 myrepo=$1
+
+mkdir $myrepo
 
 cd $myrepo
 
@@ -13,11 +16,11 @@ git commit -m "my first commit"
 cd ..
 git clone --bare $myrepo $myrepo.git
 
-mv myrepo.git $2 
+mv $myrepo.git $2 
 
 rm -rf $myrepo
 
-git clone ssh://git@127.0.0.1:2222/git-server/repos/$myrepo.git
+git clone ssh://git@127.0.0.1:2222/git-server/repos/$3/$myrepo.git
 
 echo "Done"
 
